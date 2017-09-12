00d4149a84e333ca0d7d6e16d0498434629908b9
MemgLiGuo <2264280742@qq.com>
2017-09-12 11:34:03 +0800

    第3次提交

--- app/src/main/java/com/example/dell/x_day_02demo3/MainActivity.java
+++ app/src/main/java/com/example/dell/x_day_02demo3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.x_day_02demo3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
+        initData1();
+    }
+
+    private void initData1() {
+        Log.e("TAG", "initData1:" );
     }
 
     private void initData() {
-
 
     }
 }
